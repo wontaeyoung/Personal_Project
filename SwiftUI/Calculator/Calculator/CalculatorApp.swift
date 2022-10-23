e7b1c2916d102e3b4fd2e5a4ed8dfca23966d8e5
Taeyoung Won <hexter1994@gmail.com>
2022-10-23 17:38:03 +0900

    SwiftUI 계산기 프로젝트 생성

---
+++ SwiftUI/Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by 원태영 on 2022/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
